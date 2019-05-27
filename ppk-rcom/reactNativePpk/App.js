eed98615f1072121b8586c2985fdc95c7150ffc5
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2019-05-27 16:11:14 +0300

    displaying json data, playing with basic auth

--- ppk-rcom/reactNativePpk/App.js
+++ ppk-rcom/reactNativePpk/App.js
@@ -1,47 +1,90 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView, FlatList, ActivityIndicator} from 'react-native';
+import { Base64 } from 'js-base64';
 
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props){
     super(props);
     this.state = {
-      ppks: []
+      ppks: [],
+	  isLoading: true,
+	  users: []
     }
   }
 /*
   componentDidMount(){
-    fetchPpkData();
+    return fetch("https://jsonplaceholder.typicode.com/users")
+	.then((response) => response.json())
+	.then((responseJSON) => {
+		console.log(responseJSON)
+		this.setState({
+			isLoading: false,
+			users: responseJSON
+		})
+	    }
+		)
+		.catch((err) => {
+			console.log(err);
+		})
   }
+  */
+  
 
-fetchPpkData(){
+
+componentDidMount(){
     fetch("http://194.187.110.62:15004/api/devices/state/",{
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        "Authorization": "Basic "+btoa("Yurii:123456")
+        //"Authorization": "Basic " + Base64.encode("Yurii:123456")
       }
       })
-      .then((ppks) => {
-        console.log(ppks.data);
-        this.setState({
-          ppks: ppks.data.data
-        });
-      })
+	  .then((response) => response.json())
+	  .then((responseJSON) => {
+		  console.log(responseJSON.data);
+		  this.setState({
+			isLoading: false,
+			ppks: responseJSON.data.data
+		})
+	  })      
       .catch((err) => {
         console.log(err)
       })
   }
-*/
+
 
   render() {
 
     let ppkData = Object.values(this.state.ppks);
 
+    if(this.state.isLoading){
+		return(
+		  <View style={{flex: 1, padding: 40}}>
+		    <Text style={styles.welcome}>Loading...</Text>
+            <ActivityIndicator/>
+          </View>
+		)
+	}
+
     return (
-      <View style={styles.container}>
-           <Text style={styles.welcome}>Welcome to React Native! Hello!</Text>           
+      <View style={{flex: 1, paddingTop:20}}>
+           <Text style={styles.welcome}>Welcome to React Native! Hello There!</Text> 
+		   <ScrollView>
+		   {/*<FlatList
+		       data={this.state.users}
+			   renderItem={({item}) => <Text>{item.name}, {item.email}</Text>}
+               keyExtractor={({id}, index) => id}
+		     />
+		   */}
+		   
+		     <FlatList
+		       data={ppkData}
+			   renderItem={({item}) => <Text>{item.power}, {item.door}</Text>}
+               keyExtractor={(index) =>index+1}
+		     />
+           </ScrollView> 		   
       </View>
     );
   }

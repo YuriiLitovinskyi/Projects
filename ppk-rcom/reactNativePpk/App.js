ccb1017c070c28570e98759d0defa5c1b978db9a
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2019-05-28 15:30:57 +0300

    some changes

--- ppk-rcom/reactNativePpk/App.js
+++ ppk-rcom/reactNativePpk/App.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, ScrollView, FlatList, ActivityIndicator} from 'react-native';
 import { Base64 } from 'js-base64';
+import {
+	Platform, 
+	StyleSheet, 
+	Text, 
+	View, 
+	ScrollView, 
+	FlatList, 
+	ActivityIndicator,
+	SectionList
+  } from 'react-native';
+
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -52,6 +62,7 @@ componentDidMount(){
 		)
 	}
 
+
     return (
       <View style={{flex: 1, paddingTop:20}}>
            <Text style={styles.welcome}>Devices:</Text> 
@@ -63,16 +74,44 @@ componentDidMount(){
             renderItem={({item}) => <Text>PPK Number: {item}</Text>}
            keyExtractor={(index) => index}
           />
-		    <FlatList
-          
+		  
+		  {/*	    <FlatList			
 		       data={ppkData}
-           numColumns={1}
-			     renderItem={({item, index}) => <Text>PPK: {ppk[index]}, Model: {item.model} Online: {item.online}, Power: {item.power}, Door: {item.door}, Battery: {item.accum}</Text>}
-           keyExtractor={({id}, index) => id}
-		     />
-         <Text>SectionList?</Text>
-         
-           </ScrollView> 		   
+               numColumns={1}
+			   renderItem={({item, index}) => {
+			     if (item.model === "8l"){
+					 return <Text>PPK: {ppk[index]}, Model: {item.model} Online: {item.online}, Power: {item.power}, Door: {item.door}, 
+					 Battery: {item.accum}, UK2: {item.uk2}, UK3: {item.uk3}, Rele: {item.relay2}, 
+					 Zone 1: {item.lines[1]}, Zone 2: {item.lines[2]}</Text>
+				 } else if (item.model === "4l") { 
+					 return  <Text>PPK: {ppk[index]}, Model: {item.model} Online: {item.online}, Power: {item.power}, Door: {item.door}, Battery: {item.accum}, Zone 1: {item.lines[1]}</Text>
+				 } else return <Text>Unknown device</Text>
+			   }
+			   }
+               keyExtractor={(item, index) => index}
+  />*/}
+         <Text>SectionList... try? or not...?</Text>
+           </ScrollView> 
+ 
+		 <SectionList
+		    sections={[
+				{title: "8l ppk", data: ppkData},
+				{title: "4l ppk", data: ppkData}
+			]}
+			renderItem={({item, index}) => {
+			     if (item.model === "8l"){
+					 return <Text>PPK: {ppk[index]}, Model: {item.model} Online: {item.online}, Power: {item.power}, Door: {item.door}, 
+					 Battery: {item.accum}, UK2: {item.uk2}, UK3: {item.uk3}, Rele: {item.relay2}, 
+					 Zone 1: {item.lines[1]}, Zone 2: {item.lines[2]}</Text>
+				 } else if (item.model === "4l") { 
+					 return  <Text>PPK: {ppk[index]}, Model: {item.model} Online: {item.online}, Power: {item.power}, Door: {item.door}, Battery: {item.accum}, Zone 1: {item.lines[1]}</Text>
+				 } else return <Text>Unknown device</Text>
+			   }
+			   }
+			   renderSectionHeader={({section}) => <Text>{section.title}</Text>}
+			   keyExtractor={(item) => item._id}
+             		 
+		 />		   
       </View>
     );
   }
